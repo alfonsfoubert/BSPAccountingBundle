41b16f208aa4cb598e465d1cef958254270d819e
D3r3ck <devnull@localhost>
2012-06-11 11:08:04 +0200

    Improved Encrypted data Type Loader

--- DependencyInjection/BSPAccountingExtension.php
+++ DependencyInjection/BSPAccountingExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use BSP\AccountingBundle\Type\ExtendedDataType;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -21,7 +22,7 @@ class BSPAccountingExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
+        
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load(sprintf('%s.yml', $config['db_driver']));
         $loader->load('services.yml');
@@ -29,6 +30,8 @@ class BSPAccountingExtension extends Extension
         if (isset($config['secret'])) {
         	$container->setParameter('bsp_accounting.encryption_service.secret', $config['secret']);
         }
+        
+        $this->setTypes( $config, $container );
         
         if (isset($config['account'])) {
         	$this->configureAccount($config['account'], $container);
@@ -40,5 +43,16 @@ class BSPAccountingExtension extends Extension
         if (isset($config['account'])) {
             $container->setParameter('bsp_accounting.account.class', $config['class']);
         }
+    }
+    
+    protected function setTypes( $config, $container )
+    {
+    	if ($config['db_driver'] == 'mongodb')
+    	{
+	    	if (false === \Doctrine\ODM\MongoDB\Mapping\Types\Type::hasType(ExtendedDataType::NAME)) {
+	    		ExtendedDataType::setEncryptionService($container->get('bsp_accounting.encryption_service'));
+	    		\Doctrine\ODM\MongoDB\Mapping\Types\Type::addType(ExtendedDataType::NAME, 'BSP\AccountingBundle\Type\ExtendedDataType');
+	    	}    		
+    	}
     }
 }
